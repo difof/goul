8a2842f0bb24af3d9ab0d861f83f8a1741d58b66
difof <hesyoamaster@protonmail.com>
2022-12-06 19:59:49 +0330

    added error handling to linq functions

--- generics/linq.go
+++ generics/linq.go
@@ -13,6 +13,20 @@ func SliceMap[T any, U any](slice []T, fn func(T) U) []U {
 	return result
 }
 
+// SliceMapE returns a new slice with the results of applying the given function to each element of the given slice.
+func SliceMapE[T any, U any](slice []T, fn func(T) (U, error)) (result []U, err error) {
+	result = make([]U, len(slice))
+
+	for i, v := range slice {
+		result[i], err = fn(v)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
 // SortF sorts the given slice using the given function.
 func SortF[T any](slice []T, fn func(T, T) bool) {
 	for i := 0; i < len(slice); i++ {
@@ -22,6 +36,21 @@ func SortF[T any](slice []T, fn func(T, T) bool) {
 			}
 		}
 	}
+}
+
+// SortFE sorts the given slice using the given function.
+func SortFE[T any](slice []T, fn func(T, T) (bool, error)) error {
+	for i := 0; i < len(slice); i++ {
+		for j := i + 1; j < len(slice); j++ {
+			if ok, err := fn(slice[i], slice[j]); err != nil {
+				return err
+			} else if ok {
+				slice[i], slice[j] = slice[j], slice[i]
+			}
+		}
+	}
+
+	return nil
 }
 
 // Sort sorts the given slice in ascending order.
