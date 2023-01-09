db6e9c0b683ce9c2f215152fbee0169878ccd132
difof <hesyoamaster@protonmail.com>
2023-01-09 19:28:00 +0330

    added convenience methods

--- generics/tuple.go
+++ generics/tuple.go
@@ -1,5 +1,3 @@
-//
-
 package generics
 
 type Tuple[T any, U any] struct {
@@ -10,4 +8,14 @@ type Tuple[T any, U any] struct {
 // NewTuple creates a new Tuple.
 func NewTuple[T any, U any](first T, second U) Tuple[T, U] {
 	return Tuple[T, U]{first, second}
+}
+
+// Key returns the first value of the tuple.
+func (t Tuple[T, U]) Key() T {
+	return t.First
+}
+
+// Value returns the second value of the tuple.
+func (t Tuple[T, U]) Value() U {
+	return t.Second
 }
