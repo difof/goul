cb3510346bbaeeff7e5c3bbe7e2178f4d6a78613
difof <hesyoamaster@protonmail.com>
2023-02-12 16:13:28 +0330

    added gzip decompression

--- binary/easygzip.go
+++ binary/easygzip.go
@@ -35,3 +35,35 @@ func EasyGzip(filePath string) error {
 
 	return nil
 }
+
+func EasyUnGzip(filePath string) (string, error) {
+	// Open the original file
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", fmt.Errorf("error opening file: %w", err)
+	}
+	defer file.Close()
+
+	// Create a new file with .gz extension
+	decompressedFilePath := filePath + ".decompressed"
+	decompressedFile, err := os.Create(decompressedFilePath)
+	if err != nil {
+		return "", fmt.Errorf("error creating decompressed file: %w", err)
+	}
+	defer decompressedFile.Close()
+
+	// Use gzip writer to compress the file
+	gzipReader, err := gzip.NewReader(file)
+	if err != nil {
+		return "", fmt.Errorf("error creating gzip reader: %w", err)
+	}
+	defer gzipReader.Close()
+
+	// Copy the content of the original file to the gzip writer
+	_, err = io.Copy(decompressedFile, gzipReader)
+	if err != nil {
+		return "", fmt.Errorf("error copying file to gzip writer: %w", err)
+	}
+
+	return decompressedFilePath, nil
+}
