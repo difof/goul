40f5309958bbecb5fd52f6b6b0a1c8a6c745f11a
difof <hesyoamaster@protonmail.com>
2023-02-19 23:51:14 +0330

    completely overhauled task scheduler. it's much more optimized and readable

--- task/task.go
+++ task/task.go
@@ -1,15 +1,39 @@
 package task
 
 import (
+	"context"
 	"github.com/gofrs/uuid"
 	"time"
 )
 
 // Task is passed to task handlers and contains the payload
 type Task struct {
-	config  *TaskConfig
-	Payload interface{}
-	Elapsed time.Duration
+	config  *Config
+	args    []any
+	elapsed time.Duration
+	ctx     context.Context
+	cancel  context.CancelFunc
+}
+
+func newTask(config *Config, args []any) (t *Task) {
+	t = &Task{
+		config: config,
+		args:   args,
+	}
+
+	t.ctx, t.cancel = context.WithCancel(context.Background())
+
+	return
+}
+
+// Args returns the task arguments.
+func (t *Task) Args() []any {
+	return t.args
+}
+
+// Elapsed returns the time elapsed since the last run.
+func (t *Task) Elapsed() time.Duration {
+	return t.elapsed
 }
 
 // Id returns the task id.
@@ -19,5 +43,5 @@ func (t *Task) Id() uuid.UUID {
 
 // Stop stops the task.
 func (t *Task) Stop() {
-	t.config.scheduler.removeTask(t.config.id)
+	t.cancel()
 }
--- task/task_config.go
+++ task/task_config.go
@@ -1,14 +1,12 @@
 package task
 
 import (
+	"github.com/difof/goul/concurrency"
 	"github.com/gofrs/uuid"
-	"golang.org/x/sync/semaphore"
-	"log"
 	"time"
 )
 
-type TaskHandler func(*Task) error
-type TaskErrorHandler func(error, *Task)
+type Handler func(*Task) error
 
 const (
 	unitMilliseconds = time.Millisecond
@@ -19,7 +17,23 @@ const (
 	unitWeeks        = unitDays * 7
 )
 
-type TaskConfig struct {
+// Every begins configuring a task. supply zero or one intervals. no intervals will be counted as 1
+func Every(interval ...int) *Config {
+	i := 1
+	if len(interval) > 0 {
+		i = interval[0]
+	}
+
+	return newConfig(i, false)
+}
+
+// Once begins configuring a task. sets the task to run only once. use Config.After or TaskConfig.From to set the time.
+func Once() *Config {
+	return newConfig(1, true)
+}
+
+// Config is responsible for configuring a task and scheduling it.
+type Config struct {
 	id           uuid.UUID
 	oneShot      bool
 	nextStep     time.Time // is the nextStep time for this task to run
@@ -30,188 +44,187 @@ type TaskConfig struct {
 	hour, minute int
 	from, to     time.Time
 
+	sem           *concurrency.Semaphore
+	runner        *Runner
 	task          *Task
-	scheduler     *Scheduler
-	onTick        TaskHandler
-	onFinish      TaskHandler
-	onBeforeStart TaskHandler
-	onError       TaskErrorHandler
-	sem           *semaphore.Weighted
+	onTick        Handler
+	onFinish      Handler
+	onBeforeStart Handler
 }
 
-func (tc *TaskConfig) callHandler(t *Task, f TaskHandler) {
+func newConfig(interval int, oneShot bool) (config *Config) {
+	now := time.Now()
+
+	config = &Config{
+		id:       uuid.Must(uuid.NewV4()),
+		lastRun:  now,
+		interval: time.Duration(interval),
+		weekDay:  now.Weekday(),
+		hour:     now.Hour(),
+		minute:   now.Minute(),
+		oneShot:  oneShot,
+		sem:      concurrency.NewSemaphore(1),
+	}
+
+	return
+}
+
+func (c *Config) callHandler(f Handler) error {
 	if f != nil {
-		if err := f(t); err != nil {
-			if tc.onError != nil {
-				tc.onError(err, t)
-			} else {
-				log.Printf("task error: %s", err)
-			}
+		if err := f(c.task); err != nil {
+			return err
 		}
 	}
+
+	return nil
 }
 
-// Do start the task with the supplied payload in a new goroutine.
-func (tc *TaskConfig) Do(f TaskHandler, payload ...interface{}) *TaskConfig {
-	var thePayload interface{}
-	if payload != nil && len(payload) > 0 {
-		thePayload = payload[0]
-	}
+// Do run the task with the supplied payload in a new goroutine.
+func (c *Config) Do(f Handler, args ...any) (r *Runner, err error) {
+	c.task = newTask(c, args)
+	c.onTick = f
 
-	tc.task = &Task{
-		config:  tc,
-		Payload: thePayload,
-	}
+	r, err = newRunner(c)
+	c.runner = r
 
-	tc.onTick = f
-
-	tc.scheduler.preRunTask(tc)
-
-	return tc
-}
-
-// OnError sets the error handler for the task.
-// Errors happening in tasks won't stop the task from running. Call Task.Stop to stop the task.
-func (tc *TaskConfig) OnError(f TaskErrorHandler) *TaskConfig {
-	tc.onError = f
-	return tc
+	return
 }
 
 // OnFinish sets the finish handler for the task.
-func (tc *TaskConfig) OnFinish(f TaskHandler) *TaskConfig {
-	tc.onFinish = f
-	return tc
+func (c *Config) OnFinish(f Handler) *Config {
+	c.onFinish = f
+	return c
 }
 
 // OnBeforeStart sets the handler to be called before the task starts.
-func (tc *TaskConfig) OnBeforeStart(f TaskHandler) *TaskConfig {
-	tc.onBeforeStart = f
-	return tc
+func (c *Config) OnBeforeStart(f Handler) *Config {
+	c.onBeforeStart = f
+	return c
 }
 
 // At sets the time of day to run the task.
-func (tc *TaskConfig) At(hour, minute int) *TaskConfig {
-	tc.hour = hour
-	tc.minute = minute
-	return tc
+func (c *Config) At(hour, minute int) *Config {
+	c.hour = hour
+	c.minute = minute
+	return c
 }
 
-// From sets the start time of the task.
-func (tc *TaskConfig) From(from time.Time) *TaskConfig {
-	tc.from = from
-	return tc
+// From sets the run time of the task.
+func (c *Config) From(from time.Time) *Config {
+	c.from = from
+	return c
 }
 
 // To sets the end time of the task.
-func (tc *TaskConfig) To(to time.Time) *TaskConfig {
-	tc.to = to
-	return tc
+func (c *Config) To(to time.Time) *Config {
+	c.to = to
+	return c
 }
 
 // After starts the task after the specified duration. Short hand for From(time.Now().Add(interval))
-func (tc *TaskConfig) After(interval time.Duration) *TaskConfig {
-	return tc.From(time.Now().Add(interval))
+func (c *Config) After(interval time.Duration) *Config {
+	return c.From(time.Now().Add(interval))
 }
 
 // Millisecond sets the interval to milliseconds.
-func (tc *TaskConfig) Millisecond() *TaskConfig { return tc.Milliseconds() }
+func (c *Config) Millisecond() *Config { return c.Milliseconds() }
 
 // Milliseconds is same as Millisecond.
-func (tc *TaskConfig) Milliseconds() *TaskConfig {
-	tc.unit = unitMilliseconds
-	return tc
+func (c *Config) Milliseconds() *Config {
+	c.unit = unitMilliseconds
+	return c
 }
 
 // Second sets the interval to seconds.
-func (tc *TaskConfig) Second() *TaskConfig { return tc.Seconds() }
+func (c *Config) Second() *Config { return c.Seconds() }
 
 // Seconds is same as Second.
-func (tc *TaskConfig) Seconds() *TaskConfig {
-	tc.unit = unitSeconds
-	return tc
+func (c *Config) Seconds() *Config {
+	c.unit = unitSeconds
+	return c
 }
 
 // Minute sets the interval to minutes.
-func (tc *TaskConfig) Minute() *TaskConfig { return tc.Minutes() }
+func (c *Config) Minute() *Config { return c.Minutes() }
 
 // Minutes is same as Minute.
-func (tc *TaskConfig) Minutes() *TaskConfig {
-	tc.unit = unitMinutes
-	return tc
+func (c *Config) Minutes() *Config {
+	c.unit = unitMinutes
+	return c
 }
 
 // Hour sets the interval to hours.
-func (tc *TaskConfig) Hour() *TaskConfig { return tc.Hours() }
+func (c *Config) Hour() *Config { return c.Hours() }
 
 // Hours is same as Hour.
-func (tc *TaskConfig) Hours() *TaskConfig {
-	tc.unit = unitHours
-	return tc
+func (c *Config) Hours() *Config {
+	c.unit = unitHours
+	return c
 }
 
 // Day sets the interval to days.
-func (tc *TaskConfig) Day() *TaskConfig { return tc.Days() }
+func (c *Config) Day() *Config { return c.Days() }
 
 // Days is same as Day.
-func (tc *TaskConfig) Days() *TaskConfig {
-	tc.unit = unitDays
-	return tc
+func (c *Config) Days() *Config {
+	c.unit = unitDays
+	return c
 }
 
 // Week sets the interval to weeks.
-func (tc *TaskConfig) Week() *TaskConfig { return tc.Weeks() }
+func (c *Config) Week() *Config { return c.Weeks() }
 
 // Weeks is same as Week.
-func (tc *TaskConfig) Weeks() *TaskConfig {
-	tc.unit = unitWeeks
-	return tc
+func (c *Config) Weeks() *Config {
+	c.unit = unitWeeks
+	return c
 }
 
 // Saturday sets the unit to weeks and only runs on Saturdays.
-func (tc *TaskConfig) Saturday() *TaskConfig {
-	tc.unit = unitWeeks
-	tc.weekDay = time.Saturday
-	return tc
+func (c *Config) Saturday() *Config {
+	c.unit = unitWeeks
+	c.weekDay = time.Saturday
+	return c
 }
 
 // Sunday sets the unit to weeks and only runs on Sundays.
-func (tc *TaskConfig) Sunday() *TaskConfig {
-	tc.unit = unitWeeks
-	tc.weekDay = time.Sunday
-	return tc
+func (c *Config) Sunday() *Config {
+	c.unit = unitWeeks
+	c.weekDay = time.Sunday
+	return c
 }
 
 // Monday sets the unit to weeks and only runs on Mondays.
-func (tc *TaskConfig) Monday() *TaskConfig {
-	tc.unit = unitWeeks
-	tc.weekDay = time.Monday
-	return tc
+func (c *Config) Monday() *Config {
+	c.unit = unitWeeks
+	c.weekDay = time.Monday
+	return c
 }
 
 // Tuesday sets the unit to weeks and only runs on Tuesdays.
-func (tc *TaskConfig) Tuesday() *TaskConfig {
-	tc.unit = unitWeeks
-	tc.weekDay = time.Tuesday
-	return tc
+func (c *Config) Tuesday() *Config {
+	c.unit = unitWeeks
+	c.weekDay = time.Tuesday
+	return c
 }
 
 // Wednesday sets the unit to weeks and only runs on Wednesdays.
-func (tc *TaskConfig) Wednesday() *TaskConfig {
-	tc.unit = unitWeeks
-	tc.weekDay = time.Wednesday
-	return tc
+func (c *Config) Wednesday() *Config {
+	c.unit = unitWeeks
+	c.weekDay = time.Wednesday
+	return c
 }
 
 // Thursday sets the unit to weeks and only runs on Thursdays.
-func (tc *TaskConfig) Thursday() *TaskConfig {
-	tc.unit = unitWeeks
-	tc.weekDay = time.Thursday
-	return tc
+func (c *Config) Thursday() *Config {
+	c.unit = unitWeeks
+	c.weekDay = time.Thursday
+	return c
 }
 
 // Friday sets the unit to weeks and only runs on Fridays.
-func (tc *TaskConfig) Friday() *TaskConfig {
-	tc.unit = unitWeeks
-	tc.weekDay = time.Friday
-	return tc
+func (c *Config) Friday() *Config {
+	c.unit = unitWeeks
+	c.weekDay = time.Friday
+	return c
 }
