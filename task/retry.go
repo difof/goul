8dc78a9d3aaf687ec401d9a0bc750c93c2411f4d
difof <hesyoamaster@protonmail.com>
2023-04-13 14:41:27 +0430

    improved retry signature

--- task/retry.go
+++ task/retry.go
@@ -5,28 +5,31 @@ import (
 	"time"
 )
 
+type RetryBackoff func(retry int) time.Duration
+type RetryCallback func(count int) (stop bool, err error)
+
 func Retry(
 	ctx context.Context,
-	count, delaySec int,
-	backofffn func(count int) (delaySec int),
-	fn func(count int) (stop bool, err error),
-	errfn func(err error)) (err error) {
-	tk := time.NewTicker(time.Duration(delaySec) * time.Second)
+	maxRetry int,
+	callback RetryCallback,
+	backoff RetryBackoff,
+	errHook func(err error),
+) (err error) {
+	tk := time.NewTicker(1 * time.Second)
 	defer tk.Stop()
 
 	var stop bool
-	for i := 0; i < count; i++ {
-		stop, err = fn(i)
-		if err != nil && errfn != nil {
-			errfn(err)
-			err = nil
+	for i := 0; i < maxRetry; i++ {
+		stop, err = callback(i)
+		if err != nil && errHook != nil {
+			errHook(err)
 		}
 
 		if stop || err == nil {
 			return
 		}
 
-		tk.Reset(time.Duration(backofffn(i)) * time.Second)
+		tk.Reset(backoff(i))
 		select {
 		case <-ctx.Done():
 			return
