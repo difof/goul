06c5b62bfad30333c474b611c96eda7e7d74c154
difof <hesyoamaster@protonmail.com>
2023-10-05 21:06:13 +0330

    Any and AnyF slice linq

--- generics/slice/slice_linq.go
+++ generics/slice/slice_linq.go
@@ -2,6 +2,30 @@ package slice
 
 import "github.com/difof/goul/generics"
 
+// Any returns true if any element of the given slice is equal to the given value.
+func Any[T comparable](slice []T, value T) bool {
+	for _, v := range slice {
+		if v == value {
+			return true
+		}
+	}
+
+	return false
+}
+
+// AnyF returns true if any element of the given slice satisfies the given function.
+func AnyF[T any](slice []T, fn func(T) (bool, error)) (result bool, err error) {
+	for _, v := range slice {
+		if result, err = fn(v); err != nil {
+			return
+		} else if result {
+			return
+		}
+	}
+
+	return
+}
+
 // Map returns a new slice with the results of applying the given function to each element of the given slice.
 func Map[TIn any, TOut any](slice []TIn, fn func(TIn) (TOut, error)) (result []TOut, err error) {
 	result = make([]TOut, len(slice))
@@ -53,6 +77,49 @@ func Filter[T any](slice []T, fn func(T) (bool, error)) (result []T, err error) 
 		if ok, err = fn(v); err != nil {
 			return
 		} else if ok {
+			result = append(result, v)
+		}
+	}
+
+	return
+}
+
+// Find returns the first element of the given slice for which the given function returns true.
+func Find[T any](slice []T, fn func(T) (bool, error)) (result T, ok bool, err error) {
+	for _, v := range slice {
+		if ok, err = fn(v); err != nil {
+			return
+		} else if ok {
+			result = v
+			return
+		}
+	}
+
+	return
+}
+
+// Findi returns the index of the first element of the given slice for which the given function returns true.
+func Findi[T any](slice []T, fn func(T) (bool, error)) (result int, ok bool, err error) {
+	for i, v := range slice {
+		if ok, err = fn(v); err != nil {
+			return
+		} else if ok {
+			result = i
+			return
+		}
+	}
+
+	return
+}
+
+// Remove removes the first element of the given slice for which the given function returns true.
+func Remove[T any](slice []T, fn func(T) (bool, error)) (result []T, ok bool, err error) {
+	result = make([]T, 0, len(slice))
+
+	for _, v := range slice {
+		if ok, err = fn(v); err != nil {
+			return
+		} else if !ok {
 			result = append(result, v)
 		}
 	}
