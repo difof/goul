a1c330f12f2e6134ab0292354d610e26e3c6d273
difof <hesyoamaster@protonmail.com>
2023-11-22 16:39:05 +0330

    basic map stuff

---
+++ generics/map/map_linq.go
@@ -0,0 +1,21 @@
+package _map
+
+func Keys[K comparable, V any](m map[K]V) []K {
+	keys := make([]K, 0, len(m))
+
+	for k := range m {
+		keys = append(keys, k)
+	}
+
+	return nil
+}
+
+func Values[K comparable, V any](m map[K]V) []V {
+	values := make([]V, 0, len(m))
+
+	for _, v := range m {
+		values = append(values, v)
+	}
+
+	return nil
+}
